ae9c78687c81f1992850867e7299b61a075ef2fe
Alexander Grube <alexp.grube@googlemail.com>
2021-03-18 23:59:54 +0100

    item.Value() bugged

---
+++ blockchain/blockchain.go
@@ -0,0 +1,55 @@
+package blockchain
+
+import (
+	"fmt"
+
+	"github.com/dgraph-io/badger"
+)
+
+const (
+	dbPath = "./tmp/blocks"
+)
+
+type BlockChain struct {
+	LastHash []byte
+	Database *badger.DB
+}
+
+func InitBlockChain() *BlockChain {
+	var lastHash []byte
+
+	opts := badger.DefaultOptions(dbPath)
+
+	db, err := badger.Open(opts)
+	Handle(err)
+
+	err = db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("No existing blockchain found")
+			genesis := Genesis()
+			fmt.Println("Genesis proved")
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			Handle(err)
+			err = txn.Set([]byte("lh"), genesis.Hash)
+
+			lastHash = genesis.Hash
+
+			return err
+		} else {
+			item, err := txn.Get([]byte("lh"))
+			Handle(err)
+			// https://youtu.be/szOZ3p-5YIc?t=660
+			// item.Value() changed
+			err = item.Value()
+			return err
+		}
+	})
+
+	Handle(err)
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.Blocks = append(chain.Blocks, new)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/alexander-grube/gocry
 
 go 1.16
+
+require (
+	github.com/dgraph-io/badger v1.6.2 // indirect
+	github.com/dgraph-io/ristretto v0.0.3 // indirect
+	github.com/golang/protobuf v1.5.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
+	golang.org/x/sys v0.0.0-20210317225723-c4fcb01b228e // indirect
+)
